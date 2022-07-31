283f7343389d96073c555b76c67c3d7e46a1fa4c
Baebin <baebine@naver.com>
2022-07-31 17:32:08 +0900

    내적

---
+++ src/main/java/level_1/신규아이디추천/Solution.java
@@ -0,0 +1,23 @@
+package level_1.신규아이디추천;
+
+class Solution {
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase()
+                .replaceAll("[^a-z0-9-_.]", "")
+                .replaceAll("[.]{2,}", ".")
+                .replaceAll("^[.]|[.]$", "");
+
+        if (new_id.equals(""))
+            return "aaa";
+        if (new_id.length() >= 16)
+            new_id = new_id.substring(0, 15)
+                    .replaceAll("[.]$", "");
+        else if (new_id.length() <= 2) {
+            char c = new_id.charAt(new_id.length() - 1);
+            for (int i = 0; i < 4 - new_id.length(); i++)
+                new_id += c;
+        }
+
+        return new_id;
+    }
+}
